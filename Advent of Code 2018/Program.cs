49b3093b5a6b25330c45fb44a19c3f361a587966
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-12-02 15:31:11 -0500

    Added Advent of Code 2018

---
+++ Advent of Code 2018/Program.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2018.Challenges.Day1;
+using Killerrin.Toolkit.CMD.Managers;
+using System;
+
+namespace AdventOfCode2018
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var navigationManager = NavigationManager.Instance;
+            navigationManager.OnExit += NavigationManager_OnExit;
+
+            navigationManager.Navigate(new MainMenu());
+            var currentMenu = navigationManager.Peek();
+            currentMenu.Run();
+        }
+
+        private static void NavigationManager_OnExit(object sender, object e)
+        {
+        }
+    }
+}
